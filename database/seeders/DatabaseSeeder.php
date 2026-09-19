b14ae8b53880d087770eb4c49cc6e473673d5928
davidngungila <davidngungila@gmail.com>
2026-09-19 18:41:58 +0300

    Add ShopTypeFullCatalogueSeeder with full realistic catalogue for each of 37 shop types (20 per type, 740 total)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             UnitsCategoriesSeeder::class,
             ProductCatalogueSeeder::class,
             PharmacyCatalogueSeeder::class,
+            ShopTypeFullCatalogueSeeder::class,
         ]);
     }
 }
